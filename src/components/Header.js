c42bd425e475f27afff550317cd057b4e245f194
bagusyatma <42886214+bagusyatma@users.noreply.github.com>
2024-01-30 00:20:23 +0700

    missing href homepage on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function Header() {
+  const router = useRouter();
   return (
     <div className="container m-auto" style={{ borderBottom: '1px solid #F9D4BF' }}>
       <div className="flex flex-row items-center justify-between p-4">
-        <div className="flex flex-row items-center cursor-pointer">
+        <div className="flex flex-row items-center cursor-pointer" onClick={() => router.push('/')}>
           <Image src="/logo.png" width={56} height={56} alt="mealapar-logo" />
           <div className="ml-2 text-2xl font-bold">Mealapar!</div>
         </div>
